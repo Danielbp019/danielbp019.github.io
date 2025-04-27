351fa5284e3c52783af2c020e521331c747adbba
Daniel Bautista P. <76835848+Danielbp019@users.noreply.github.com>
2025-04-26 21:51:10 -0500

    Actualizaciones varias
    Actualizo: boostrap, normalize, separación de luciérnagas en otro archivo, modales en git y error, eliminación de estilos sin uso, eliminación de datos en index poco relevantes, cambio de música por instrumental, firma y audio en style css y correcciones en fondos.

--- assets/js/mis.js
+++ assets/js/mis.js
@@ -7,34 +7,44 @@ audio.volume = 0.2;
 
 // Esconder elemento por id si el navegador es movil
 function hideForMobile() {
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(
-        navigator.userAgent)) {
-        document.getElementById('errorbroma').style.display = "none";
-    }
+  if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(
+      navigator.userAgent
+    )
+  ) {
+    document.getElementById("errorbroma").style.display = "none";
+  }
 }
 window.onload = hideForMobile;
 
-// Función para mostrar un mensaje de confirmación
+// Función para mostrar el modal de confirmación de GitHub
 function mostrarConfirmacionGithub() {
-    const confirmacion = confirm("¿Estás seguro de que deseas visitar este enlace?");
-    if (confirmacion) {
-        // Si el usuario confirma, redirige al enlace
-        const url = 'https://github.com/Danielbp019';
-        window.open(url, '_blank');
-    }
+  const modal = new bootstrap.Modal(document.getElementById("modalGithub"));
+  modal.show();
+
+  // Manejar el clic en el botón de confirmación
+  const confirmarBtn = document.getElementById("confirmarGithub");
+  confirmarBtn.onclick = function () {
+    window.open("https://github.com/Danielbp019", "_blank");
+    modal.hide();
+  };
 }
 
-// Función para mostrar un mensaje de confirmación
+// Función para mostrar el modal de confirmación de Error
 function mostrarConfirmacionerror() {
-    const confirmacion = confirm("¿Estás seguro de pulsar este botón? Yo NO lo haria.");
-    if (confirmacion) {
-        // Si el usuario confirma, redirige al enlace
-        window.location.href = "error.html";
-    }
+  const modal = new bootstrap.Modal(document.getElementById("modalError"));
+  modal.show();
+
+  // Manejar el clic en el botón de confirmación
+  const confirmarBtn = document.getElementById("confirmarError");
+  confirmarBtn.onclick = function () {
+    window.location.href = "error.html";
+    modal.hide();
+  };
 }
 
 // Enable tooltips everywhere
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
